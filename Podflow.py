2f69b603510b1ff11828742a817994075aba287e
gruel_zxz <zhuxizhouzxz@gmail.com>
2023-09-17 10:39:58 +0000

    202309171839

--- Podflow.py
+++ Podflow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[28]:
+# In[77]:
 
 
 import os
@@ -36,7 +36,7 @@ default_config = {
 }
 
 
-# In[29]:
+# In[78]:
 
 
 # 文件保存模块
@@ -52,7 +52,7 @@ def file_save(content, file_name, folder=None):
         file.write(content)
 
 
-# In[30]:
+# In[79]:
 
 
 #日志模块
@@ -78,7 +78,7 @@ def write_log(log, suffix=None):
         print(f"{formatted_time_mini}|{log}")
 
 
-# In[31]:
+# In[80]:
 
 
 # 查看requests模块是否安装，并安装
@@ -94,7 +94,7 @@ except ImportError:
         sys.exit(0)
 
 
-# In[32]:
+# In[81]:
 
 
 # HTTP GET请求重试模块
@@ -112,7 +112,7 @@ def get_with_retry(url, name, max_retries=10, retry_delay=6):
     return None
 
 
-# In[33]:
+# In[82]:
 
 
 # 安装库模块
@@ -152,7 +152,7 @@ def library_install(library):
             write_log(f"{library}安装失败")
 
 
-# In[34]:
+# In[83]:
 
 
 # 安装/更新yt-dlp，并加载
@@ -162,7 +162,21 @@ import yt_dlp
 library_install("RangeHTTPServer")
 
 
-# In[35]:
+# In[84]:
+
+
+# 格式化时间
+def time_format(duration):
+    hours, remaining_seconds = divmod(int(duration), 3600)
+    minutes = remaining_seconds // 60
+    remaining_seconds = remaining_seconds % 60
+    if hours > 1:
+        return '{:02}:{:02}:{:02}'.format(hours, minutes, remaining_seconds)
+    else:
+        return '{:02}:{:02}'.format(minutes, remaining_seconds)
+
+
+# In[85]:
 
 
 # 获取视频时长模块
@@ -236,7 +250,7 @@ def download_video(video_url, output_dir, output_format, video_website, format_c
         return video_url
 
 
-# In[36]:
+# In[86]:
 
 
 # 视频完整下载模块
@@ -268,7 +282,7 @@ def dl_retry_video(video_url, output_dir, output_format, retry_count, video_webs
     return yt_id_failed
 
 
-# In[37]:
+# In[87]:
 
 
 # 构建文件夹模块
@@ -279,7 +293,7 @@ def folder_build(folder_name):
         write_log(f"文件夹{folder_name}创建成功")
 
 
-# In[38]:
+# In[88]:
 
 
 # 检查当前文件夹中是否存在config.json文件
@@ -301,7 +315,7 @@ else:
         sys.exit(0)
 
 
-# In[39]:
+# In[89]:
 
 
 # 对retry_count进行纠正
@@ -340,7 +354,7 @@ if ('category' not in config):
     config['category'] = default_config["category"]
 
 
-# In[40]:
+# In[90]:
 
 
 # 从配置文件中获取YouTube的频道
@@ -359,14 +373,14 @@ else:
     write_log("bilibili频道信息不存在")
 
 
-# In[41]:
+# In[91]:
 
 
 # 构建文件夹channel_id
 folder_build("channel_id")
 
 
-# In[42]:
+# In[92]:
 
 
 # 视频分辨率变量
@@ -435,7 +449,7 @@ for channelid_youtube_key, channelid_youtube_value in channelid_youtube_copy.ite
             channelid_youtube[channelid_youtube_key]['media'] = "m4a"
 
 
-# In[43]:
+# In[93]:
 
 
 # 读取youtube频道的id
@@ -452,7 +466,7 @@ else:
     channelid_bilibili_ids = None
 
 
-# In[44]:
+# In[94]:
 
 
 # 更新Youtube频道xml
@@ -504,7 +518,7 @@ if channelid_youtube_ids_update:
     write_log(f"需更新的YouTube频道:{', '.join(channelid_youtube_ids_update.values())}")
 
 
-# In[45]:
+# In[95]:
 
 
 # 下载YouTube视频
@@ -526,7 +540,7 @@ for ytid_key, ytid_value in youtube_content_ytid_update.items():
             write_log(f"{channelid_youtube_ids[ytid_key]}|{yt_id}无法下载")
 
 
-# In[46]:
+# In[96]:
 
 
 #生成XML模块
@@ -566,7 +580,7 @@ def xml_rss(title,link,description,category,icon,items):
 </rss>'''
 
 
-# In[47]:
+# In[97]:
 
 
 # 生成item模块
@@ -592,13 +606,7 @@ def xml_item(video_url, output_dir, video_website, channelid_title,title, descri
         output_format = "m4a"
         video_type = "audio/x-m4a"
     # 获取文件时长
-    hours, remaining_seconds = divmod(get_duration_ffprobe(f"{output_dir}/{video_url}.{output_format}"), 3600)
-    minutes = remaining_seconds // 60
-    remaining_seconds = remaining_seconds % 60
-    if hours > 1:
-        duration = '{:02}:{:02}:{:02}'.format(hours, minutes, remaining_seconds)
-    else:
-        duration = '{:02}:{:02}'.format(minutes, remaining_seconds)
+    duration = time_format(get_duration_ffprobe(f"{output_dir}/{video_url}.{output_format}"))
     # 回显对应的item
     return f'''
         <item>
@@ -619,7 +627,7 @@ def xml_item(video_url, output_dir, video_website, channelid_title,title, descri
 '''
 
 
-# In[48]:
+# In[98]:
 
 
 # 生成YouTube的item模块
@@ -650,7 +658,7 @@ def youtube_xml_item(entry):
     )
 
 
-# In[49]:
+# In[99]:
 
 
 # 生成原有的item模块
@@ -696,7 +704,7 @@ def xml_original_item(original_item):
 '''
 
 
-# In[50]:
+# In[100]:
 
 
 # 获取原始xml文件
@@ -717,14 +725,14 @@ except FileNotFoundError:  #文件不存在直接更新
     write_log("原始rss文件不存在, 无法保留原有节目")
 
 
-# In[51]:
+# In[101]:
 
 
 # 构建文件夹channel_rss
 folder_build("channel_rss")
 
 
-# In[52]:
+# In[102]:
 
 
 # 生成YouTube对应channel的需更新的items模块
@@ -784,7 +792,7 @@ def youtube_xml_items(output_dir):
     return items
 
 
-# In[53]:
+# In[103]:
 
 
 # 生成主rss
@@ -801,7 +809,7 @@ file_save(xml_rss(config["title"], config["link"], config["description"], config
 write_log("主播客已更新", f"地址: {config['url']}/{config['title']}.xml")
 
 
-# In[54]:
+# In[104]:
 
 
 # 删除多余媒体文件模块
@@ -812,7 +820,7 @@ def remove_file(output_dir):
             write_log(f"{channelid_youtube_ids[output_dir]}|{file_name}已删除")
 
 
-# In[55]:
+# In[105]:
 
 
 # 补全缺失的媒体文件模块
@@ -831,7 +839,7 @@ def make_up_file(output_dir):
                 write_log(f"{channelid_youtube_ids[file_name.split('.')[0]]}|{file_name.split('.')[0]}无法下载")
 
 
-# In[56]:
+# In[106]:
 
 
 # 删除不在rss中的媒体文件
@@ -839,7 +847,7 @@ for output_dir in channelid_youtube_ids:
     remove_file(output_dir)
 
 
-# In[57]:
+# In[107]:
 
 
 # 补全在rss中缺失的媒体文件
