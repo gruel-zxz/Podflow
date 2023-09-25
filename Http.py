eec34629712654e514509170c513208973f9079b
zxz-outlook <145938930+zxz-outlook@users.noreply.github.com>
2023-09-25 03:27:14 +0000

    202309251127

--- Http.py
+++ Http.py
@@ -3,10 +3,8 @@ import time
 
 # 启动 RangeHTTPServer
 server_process = subprocess.Popen(["python3", "-m", "http.server", "--cgi"])
-
-# 延时三分钟
-time.sleep(180)
-
+# 延时
+time.sleep(60)
 # 关闭服务器
 server_process.terminate()
 
