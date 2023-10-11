47190a0ec99bda646e8ae438f9f94e312ff5afc4
zxz-outlook <145938930+zxz-outlook@users.noreply.github.com>
2023-10-11 13:20:48 +0000

    202310112120

--- Podflow.py
+++ Podflow.py
@@ -424,18 +424,19 @@ def dl_aideo_video(video_url, output_dir, output_format, video_format, retry_cou
     else:
         video_write_log = video_url
     id_duration = video_format[0]
-    print(f"{datetime.now().strftime('%H:%M:%S')}|{video_write_log} \033[34m开始下载\033[0m")
+    print(f"{datetime.now().strftime('%H:%M:%S')}|{video_write_log} \033[34m开始下载\033[0m", end = "")
     if output_format == "m4a":
+        print(f" \033[97m{video_format[1]}\033[0m")
         yt_id_failed = dl_retry_video(video_url, output_dir, "m4a", video_format[1], id_duration, retry_count, video_website, video_write_log, format_code, "")
-        print(f" \033[97m{video_format[1]}\033[0m", end = "")
     else:
-        print(f"{datetime.now().strftime('%H:%M:%S')}|\033[34m开始视频部分下载\033[0m")
+        print()
+        print(f"{datetime.now().strftime('%H:%M:%S')}|\033[34m开始视频部分下载\033[0m", end = "")
+        print(f" \033[97m{video_format[2]}\033[0m")
         yt_id_failed = dl_retry_video(video_url, output_dir, "mp4", video_format[2], id_duration, retry_count, video_website, video_write_log, format_code, ".part")
-        print(f" \033[97m{video_format[2]}\033[0m", end = "")
         if yt_id_failed is None:
-            print(f"{datetime.now().strftime('%H:%M:%S')}|\033[34m开始音频部分下载\033[0m")
+            print(f"{datetime.now().strftime('%H:%M:%S')}|\033[34m开始音频部分下载\033[0m", end = "")
+            print(f" \033[97m{video_format[2]}\033[0m")
             yt_id_failed = dl_retry_video(video_url, output_dir, "m4a", video_format[1], id_duration, retry_count, video_website, video_write_log, format_code, ".part")
-            print(f" \033[97m{video_format[1]}\033[0m", end = "")
             if yt_id_failed is None:
                 print(f"{datetime.now().strftime('%H:%M:%S')}|\033[34m开始合成\033[0m")
                 # 构建FFmpeg命令
