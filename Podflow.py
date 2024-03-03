e947bb79d0efc3b14aa6fb332dacccc0645b6f9a
zxz-outlook <145938930+zxz-outlook@users.noreply.github.com>
2024-03-03 05:17:52 +0000

    202403031317

--- Podflow.py
+++ Podflow.py
@@ -179,9 +179,9 @@ def http_client(url, name, max_retries=10, retry_delay=4, headers_possess=False,
     for num in range(max_retries):
         try:
             if mode == "get":
-                response = session.get(url, timeout=3)
+                response = session.get(url, timeout=8)
             else:
-                response = session.post(url, timeout=3)
+                response = session.post(url, timeout=8)
             response.raise_for_status()
         except Exception as http_get_error:
             if response is not None and response.status_code in {404}:
