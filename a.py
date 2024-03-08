f4044c64b8893dbb0a06dedccd8c353c2d84d66d
zxz-outlook <zhuxizhouzxz@outlook.com>
2024-03-08 23:24:52 +0800

    202403092324

--- a.py
+++ a.py
@@ -5,7 +5,7 @@ import time
 # 运行简单的 HTTP 服务器
 server_process = subprocess.Popen(["python3", "-m", "RangeHTTPServer"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-def server_process_print(stop_flag):
+def server_process_print(server_process_print_flag):
     need_keep = ""
     while True:
         output = server_process.stdout.readline().decode().strip()
@@ -13,28 +13,28 @@ def server_process_print(stop_flag):
             need_keep = output
         else:
             need_keep += f"\n{output}"
-        if stop_flag[0] == "keep":
+        if server_process_print_flag[0] == "keep":
             print(need_keep)
             need_keep = ""
-        if stop_flag[0] == "end":
+        if server_process_print_flag[0] == "end":
             break
 
-def server_process_stop(stop_flag):
+def server_process_stop(server_process_print_flag):
     time.sleep(10)  # 等待一段时间后终止服务器
     # 关闭 HTTP 服务器进程
-    stop_flag[0] = "pause"
+    server_process_print_flag[0] = "pause"
     time.sleep(30)
-    stop_flag[0] = "keep"
+    server_process_print_flag[0] = "keep"
     time.sleep(30)
     server_process.terminate()
-    stop_flag[0] = "end"
+    server_process_print_flag[0] = "end"
 
 # 创建共享的标志变量
-stop_flag = ["keep"]
+server_process_print_flag = ["keep"]
 
 # 创建两个线程分别执行输出和终止操作
-prepare_1 = threading.Thread(target=server_process_print, args=(stop_flag,))
-prepare_2 = threading.Thread(target=server_process_stop, args=(stop_flag,))
+prepare_1 = threading.Thread(target=server_process_print, args=(server_process_print_flag,))
+prepare_2 = threading.Thread(target=server_process_stop, args=(server_process_print_flag,))
 
 # 启动两个线程
 prepare_1.start()
