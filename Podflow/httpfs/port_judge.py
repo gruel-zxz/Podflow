7499f0889c3d55585553411013758a12a14ce05f
gruel-zxz <zhuxizhouzxz@gmail.com>
2025-03-02 00:51:09 +0800

    优化端口是否占用判断

--- Podflow/httpfs/port_judge.py
+++ Podflow/httpfs/port_judge.py
@@ -4,18 +4,11 @@
 import socket
 
 
-def port_judge(host, port):
-    # 创建一个新的 socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # 设置 socket 为可重用
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def port_judge(hostip, port):
     try:
-        # 尝试绑定到指定端口
-        sock.bind((host, port))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((hostip, port))
+            return True
     except OSError:
-        # 如果绑定失败，说明端口被占用
         return False
-    else:
-        # 如果绑定成功，端口可用，关闭 socket
-        sock.close()
-        return True
