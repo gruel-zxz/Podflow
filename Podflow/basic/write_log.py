9c588e20db2543044c996288a5552ba465e93266
gruel-zxz <zhuxizhouzxz@gmail.com>
2025-03-16 03:22:07 +0800

    修改日志顺序，改为顺序，减少资源浪费

--- Podflow/basic/write_log.py
+++ Podflow/basic/write_log.py
@@ -3,7 +3,6 @@
 
 import re
 from datetime import datetime
-from Podflow.basic.file_save import file_save
 
 
 # 日志模块
@@ -19,26 +18,18 @@ def write_log(
     current_time = datetime.now()
     # 格式化输出, 只保留年月日时分秒
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    # 打开文件, 并读取原有内容
-    try:
-        with open(file_name, "r", encoding="utf-8") as file:
-            contents = file.read()
-    except FileNotFoundError:
-        contents = ""
     # 将新的日志内容添加在原有内容之前
     log_in = re.sub(r"\033\[[0-9;]+m", "", log)
     log_in = re.sub(r"\n", "", log_in)
-    if only_log:
-        only_log = re.sub(r"\033\[[0-9;]+m", "", str(only_log))
-    else:
-        only_log = ""
+    only_log = re.sub(r"\033\[[0-9;]+m", "", str(only_log)) if only_log else ""
     new_contents = (
-        f"{formatted_time} {log_in}{only_log}\n{contents}"
+        f"{formatted_time} {log_in}{only_log}\n"
         if only_log
-        else f"{formatted_time} {log_in}\n{contents}"
+        else f"{formatted_time} {log_in}\n"
     )
     # 将新的日志内容写入文件
-    file_save(new_contents, file_name)
+    with open(file_name, "a", encoding="utf-8") as file:
+        file.write(new_contents)
     if display:
         formatted_time_mini = current_time.strftime("%H:%M:%S")
         log_print = f"{formatted_time_mini}|{log}" if time_display else f"{log}"
