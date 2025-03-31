390ec9c12e2a85801e7864b47fbf51c302c98d37
gruel-zxz <zhuxizhouzxz@gmail.com>
2025-03-31 17:30:01 +0800

    优化index显示

--- podflow/httpfs/ansi_to_html.py
+++ podflow/httpfs/ansi_to_html.py
@@ -8,22 +8,22 @@ import html
 def ansi_to_html(ansi_text):
     html_output = ""
     ansi_codes = {
-        "\033[30m": "color: black;",  # 黑色
-        "\033[31m": "color: red;",  # 红色
-        "\033[32m": "color: green;",  # 绿色
-        "\033[33m": "color: yellow;",  # 黄色
-        "\033[34m": "color: blue;",  # 蓝色
-        "\033[35m": "color: magenta;",  # 品红
-        "\033[36m": "color: cyan;",  # 青色
-        "\033[37m": "color: white;",  # 白色
-        "\033[90m": "color: gray;",  # 亮黑色 (通常显示为灰色)
-        "\033[91m": "color: #ff69b4;",  # 亮红色 (例如：热粉色)
-        "\033[92m": "color: #90ee90;",  # 亮绿色 (例如：浅绿色)
-        "\033[93m": "color: #ffff00;",  # 亮黄色 (通常与黄色相同)
-        "\033[94m": "color: #add8e6;",  # 亮蓝色 (例如：浅蓝色)
-        "\033[95m": "color: #ff00ff;",  # 亮品红 (通常与品红相同)
-        "\033[96m": "color: #00ffff;",  # 亮青色 (通常与青色相同)
-        "\033[97m": "color: #f0f8ff;",  # 亮白色 (例如：爱丽丝蓝)
+        "\033[30m": ".ansi-30m;",  # 黑色
+        "\033[31m": ".ansi-31m;",  # 红色
+        "\033[32m": ".ansi-32m;",  # 绿色
+        "\033[33m": ".ansi-33m;",  # 黄色
+        "\033[34m": ".ansi-34m;",  # 蓝色
+        "\033[35m": ".ansi-35m;",  # 品红
+        "\033[36m": ".ansi-36m;",  # 青色
+        "\033[37m": ".ansi-37m;",  # 白色
+        "\033[90m": ".ansi-90m;",  # 亮黑色 (通常显示为灰色)
+        "\033[91m": ".ansi-91m;",  # 亮红色 (例如：热粉色)
+        "\033[92m": ".ansi-92m;",  # 亮绿色 (例如：浅绿色)
+        "\033[93m": ".ansi-93m;",  # 亮黄色 (通常与黄色相同)
+        "\033[94m": ".ansi-94m;",  # 亮蓝色 (例如：浅蓝色)
+        "\033[95m": ".ansi-95m;",  # 亮品红 (通常与品红相同)
+        "\033[96m": ".ansi-96m;",  # 亮青色 (通常与青色相同)
+        "\033[97m": ".ansi-97m;",  # 亮白色 (例如：爱丽丝蓝)
         "\033[0m": "",  # 重置
     }
     inside_span = False
@@ -48,5 +48,4 @@ def ansi_to_html(ansi_text):
     if inside_span:
         html_output += "</span>"
 
-    #html_output = html_output.replace("\n", "</p><p>")
     return html_output
