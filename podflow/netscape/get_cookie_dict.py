11636e6817924a580008ffde8da6f965fdda05f8
gruel-zxz <zhuxizhouzxz@gmail.com>
2025-04-13 11:56:21 +0800

    更新油管登录显示

--- podflow/netscape/get_cookie_dict.py
+++ podflow/netscape/get_cookie_dict.py
@@ -1,8 +1,8 @@
 # podflow/Netscape/get_cookie_dict.py
 # coding: utf-8
 
-from datetime import datetime
 from http.cookiejar import LoadError, MozillaCookieJar
+from podflow.basic.time_print import time_print
 
 
 # 将Netscape转Dict模块
@@ -14,8 +14,8 @@ def get_cookie_dict(file):
         cookie_jar.load(ignore_discard=True)
         return {cookie.name: cookie.value for cookie in cookie_jar}
     except FileNotFoundError:
-        print(f"{datetime.now().strftime('%H:%M:%S')}|{parts[-1]}文件不存在")
+        time_print(f"{parts[-1]}文件不存在")
         return None
     except LoadError:
-        print(f"{datetime.now().strftime('%H:%M:%S')}|{parts[-1]}文件错误")
+        time_print(f"{parts[-1]}文件错误")
         return None
