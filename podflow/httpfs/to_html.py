d29d461dfd539a6295351f658db1dda7c12d1650
gruel-zxz <zhuxizhouzxz@gmail.com>
2025-04-15 16:58:59 +0800

    修复index显示

--- podflow/httpfs/to_html.py
+++ podflow/httpfs/to_html.py
@@ -16,14 +16,14 @@ def ansi_to_html(ansi_text):
         "\033[35m": "ansi-35m",  # 品红
         "\033[36m": "ansi-36m",  # 青色
         "\033[37m": "ansi-37m",  # 白色
-        "\033[90m": "ansi-90m",  # 亮黑色 (通常显示为灰色)
-        "\033[91m": "ansi-91m",  # 亮红色 (例如：热粉色)
-        "\033[92m": "ansi-92m",  # 亮绿色 (例如：浅绿色)
-        "\033[93m": "ansi-93m",  # 亮黄色 (通常与黄色相同)
-        "\033[94m": "ansi-94m",  # 亮蓝色 (例如：浅蓝色)
-        "\033[95m": "ansi-95m",  # 亮品红 (通常与品红相同)
-        "\033[96m": "ansi-96m",  # 亮青色 (通常与青色相同)
-        "\033[97m": "ansi-97m",  # 亮白色 (例如：爱丽丝蓝)
+        "\033[90m": "ansi-90m",  # 亮黑色
+        "\033[91m": "ansi-91m",  # 亮红色 
+        "\033[92m": "ansi-92m",  # 亮绿色
+        "\033[93m": "ansi-93m",  # 亮黄色
+        "\033[94m": "ansi-94m",  # 亮蓝色
+        "\033[95m": "ansi-95m",  # 亮品红
+        "\033[96m": "ansi-96m",  # 亮青色
+        "\033[97m": "ansi-97m",  # 亮白色
         "\033[0m": "",  # 重置
     }
     inside_span = False
@@ -46,9 +46,8 @@ def ansi_to_html(ansi_text):
 
     if inside_span:
         html_output += "</span>"
-    html_output = re.sub(r'^\n', '', html_output)
-    html_output = re.sub(r'\n+$', '', html_output)
-    html_output = re.sub(r' +$', '', html_output)
+    html_output = re.sub(r'^\s', '', html_output)
+    html_output = re.sub(r'\s+$', '', html_output)
     return html_output
 
 
